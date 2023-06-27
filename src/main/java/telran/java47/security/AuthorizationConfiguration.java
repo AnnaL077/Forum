3f01dd02f48d83d0fee6ff4abfe010eeec3669d3
admin <admin@AVLivshits-h.lan>
2023-06-27 10:52:03 +0300

    End of classwork 26.06

--- src/main/java/telran/java47/security/AuthorizationConfiguration.java
+++ src/main/java/telran/java47/security/AuthorizationConfiguration.java
@@ -2,7 +2,9 @@ package telran.java47.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,27 @@ public class AuthorizationConfiguration {
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http.httpBasic();
 		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests(authorize -> authorize
+				.mvcMatchers("/account/register", "/forum/posts**")
+					.permitAll()
+				.mvcMatchers("/account/user/{login}/role/{role}")
+					.hasRole("ADMINISTRATOR")
+				.mvcMatchers(HttpMethod.PUT, "/account/user/{login}")
+					.access("#login == authentication.name")
+				.mvcMatchers(HttpMethod.DELETE, "/account/user/{login}")
+					.access("#login == authentication.name or hasRole('ADMINISTRATOR')")
+				.mvcMatchers(HttpMethod.POST, "/forum/post/{author}")
+					.access("#author == authentication.name")
+				.mvcMatchers(HttpMethod.PUT, "/forum/post/{id}/comment/{author}")
+					.access("#author == authentication.name")
+				.mvcMatchers(HttpMethod.PUT, "/forum/post/{id}")
+					.access("@customSecurity.checkPostAuthor(#id, authentication.name)")
+				.mvcMatchers(HttpMethod.DELETE, "/forum/post/{id}")
+					.access("@customSecurity.checkPostAuthor(#id, authentication.name) or hasRole('MODERATOR')")
+				.anyRequest()
+					.authenticated()
+				);
 		
 		return http.build();
 	}
